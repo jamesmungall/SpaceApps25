c7b0c3349b894b8ac58c7d1625e17d257ea8c0dd
jamesmungall <preanuts26@gmail.com>
2025-10-04 11:41:57 +0100

    Create display_image.py

---
+++ scripts/display_image.py
@@ -0,0 +1,17 @@
+'''
+Once the file exists on the local machine (in this case "Hubble_Arp105.jpg"), you display it in GaiaSky by running this function.
+
+'''
+
+def j_display_image(filename):
+    # url = "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/galaxies/interacting/Hubble_Arp105_WFC3_mstr_flat_cont_FINAL2.jpg"
+    # urllib.request.urlretrieve(url, "Hubble_Arp105.jpg")
+    if os.path.exists(filename):
+        print("File exists!")
+    else:
+        print("File does not exist.")
+    ui.preload_texture(os.path.abspath(filename))
+    ui.display_image(1, os.path.abspath(filename), 0.0, 0.0)
+
+  # Example usage
+    # j_display_image("Hubble_Arp105.jpg")
