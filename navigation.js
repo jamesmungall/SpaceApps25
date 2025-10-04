be4f880973c2b52b9d06dc065ad3bf38e1a73649
jamesmungall <preanuts26@gmail.com>
2025-10-04 11:21:38 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -3,11 +3,8 @@ function injectCommonHtml() {
         <header>
         <nav>
     <a href="index.html">Home</a>
-    <a href="plotly_chart2.html">Plotly Chart 2</a>
-    <a href="ellipse.html">Ellipse</a>
-    <a href="stars_0_23pc.csv">CSV for stars within 23pc</a>
-    <a href="read_json.html">Read JSON</a>
-	</nav>
+    <a href="gaia_test_3.py">gaia_test_3.py</a>
+    </nav>
         </header>
     `;
 
