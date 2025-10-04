25a5568652ab0e3e4218ca45bc587979340dfa55
jamesmungall <preanuts26@gmail.com>
2025-10-04 11:59:13 +0100

    Update gaia_test_3.py

--- scripts/gaia_test_3.py
+++ scripts/gaia_test_3.py
@@ -26,14 +26,34 @@ refsys = apiv2.refsys
 
 # We can now use the modules
 
-camera.focus_mode("earth")
+def j_orientate_to_ra_then_dec(final_ra, final_dec):
+    camera.set_fov(100.0)
+    time_for_transition = 1.0
+    sleep_transition_factor = 1.5
+    camera_position = [0.0, 0.0, 0.0]
+    sync = False
+
+    def j_do_transition():
+        dirn = np.array(refsys.equatorial_to_cartesian(ra, dec, 1.0E9))
+        camera.transition(camera_position, dirn, up_vector, time_for_transition, sync)
+        base.sleep(time_for_transition * sleep_transition_factor)
+
+    ra = 0.0
+    dec = 0.0
+    up_vector = [0.0, 1.0, 0.0]
+    j_do_transition()
+
+    ra = final_ra
+    j_do_transition()
+
+    dec = final_dec
+    j_do_transition()
+
+j_orientate_to_ra_then_dec(290.0, 30.0)
 
 # End script neatly
 base.settings_backup()
 base.sleep(20.0)
-
-
-
 ui.remove_all_objects()
 base.settings_restore()
 # Remember to shut down the connection before exiting
