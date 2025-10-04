281ce000b361ab5d98322d74e6f74b6ca2ca47ee
jamesmungall <preanuts26@gmail.com>
2025-10-04 11:38:05 +0100

    Create import_image_file.py

---
+++ scripts/import_image_file.py
@@ -0,0 +1,3 @@
+import urllib.request
+url = "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/galaxies/interacting/Hubble_Arp105_WFC3_mstr_flat_cont_FINAL2.jpg"
+urllib.request.urlretrieve(url, "Hubble_Arp105.jpg")
