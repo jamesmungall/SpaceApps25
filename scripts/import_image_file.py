ff51d627bda5b3301c5ab8e91bca09067794497d
jamesmungall <preanuts26@gmail.com>
2025-10-04 11:39:01 +0100

    Update import_image_file.py

--- scripts/import_image_file.py
+++ scripts/import_image_file.py
@@ -1,3 +1,7 @@
+'''
+Saves the image in the url to the local computer and save it as the file name given.
+
+'''
 import urllib.request
 url = "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/galaxies/interacting/Hubble_Arp105_WFC3_mstr_flat_cont_FINAL2.jpg"
 urllib.request.urlretrieve(url, "Hubble_Arp105.jpg")
