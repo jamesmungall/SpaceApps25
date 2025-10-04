d9eff7710cb6c180d81552429574b65f2863461b
jamesmungall <preanuts26@gmail.com>
2025-10-04 11:43:05 +0100

    Update display_image.py

--- scripts/display_image.py
+++ scripts/display_image.py
@@ -2,7 +2,7 @@
 Once the file exists on the local machine (in this case "Hubble_Arp105.jpg"), you display it in GaiaSky by running this function.
 
 '''
-
+import os
 def j_display_image(filename):
     # url = "https://assets.science.nasa.gov/dynamicimage/assets/science/missions/hubble/galaxies/interacting/Hubble_Arp105_WFC3_mstr_flat_cont_FINAL2.jpg"
     # urllib.request.urlretrieve(url, "Hubble_Arp105.jpg")
